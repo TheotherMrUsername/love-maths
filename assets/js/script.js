acbcd5ce708392d390d04096892c988de0e56e23
TheotherMrUsername <danielwraight067@gmail.com>
2022-03-10 21:54:10 +0000

    added function code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
-function runGame(){
+//wait for the DOM to finish loading befpre running the game 
+//Get the button Elements and add event listneers ot them. 
 
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+/**
+ * The main game "loop", called when the script is first loaded 
+ * amd after the user's answr has been processed
+ */
+function runGame(){
+//Creates two random numbers between 1 and 25.
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
+
 
 function checkAnswer(){
 
 }
+
+
 
 function calculateCorrectAnswer(){
 
@@ -27,5 +53,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
